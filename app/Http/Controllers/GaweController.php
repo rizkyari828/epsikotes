462ea0d5dc3dac846562f2fc2ee1a31afd334207
Naufal Fachrian <naufal.fachrian@gmail.com>
2020-06-23 00:31:42 +0700

    Update Gawecontroller

---
+++ app/Http/Controllers/GaweController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Model\Applicant;
+use Illuminate\Http\Request;
+
+class GaweController extends Controller
+{
+    public function createSchedule(Request $request)
+    {
+        return $request;
+    }
+}
